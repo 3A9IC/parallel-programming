72a198c77109d1cd4688bb11255d1b9398f5a784
AnnaAndreevna <ryakina1994@mail.ru>
2017-11-16 13:20:28 +0500

    Add files via upload
    Спарсила страницу

---
+++ NameAnya.py
@@ -0,0 +1,25 @@
+# получение текста из инета и
+# распечатывание всех существительных
+
+from bs4 import BeautifulSoup as BS
+from requests import get
+from pymorphy2 import MorphAnalyzer as MA
+from nltk.tokenize import PunktSentenceTokenizer as PST
+from nltk.tokenize import WordPunctTokenizer as WPT
+
+ma = MA()
+st = PST()
+wt = WPT()
+
+
+try:
+	my_file = open("some.txt", "r", encoding='utf-8')
+	text = my_file.read()
+except FileNotFoundError:
+	text = BS(get("http://lib.ru/KRAPIWIN/airplane.txt").content, fromEncoding="windows-1251").text
+	my_file = open("some.txt", "w", encoding='utf-8')
+	my_file.write(text)
+
+
+			
+print(text)
